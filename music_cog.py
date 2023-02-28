5810b5898bd2087c2f74160c81e9ac5aff4d8896
GOGOJO <joshualu76@gmail.com>
2023-02-28 14:00:22 -0800

    added functions

--- music_cog.py
+++ music_cog.py
@@ -2,8 +2,8 @@ import discord
 from discord.ext import commands
 import youtube_dl
 class music_cog(commands.Cog):
-    def __init__(self, client):
-        self.bot = client
+    def __init__(self, bot):
+        self.bot = bot
 
     @commands.command()
     async def join(self,ctx):
@@ -23,7 +23,13 @@ class music_cog(commands.Cog):
 
     @commands.command(aliases=['p'])
     async def play(self,ctx, url):
-        await ctx.author.voice.channel.connect()
+        if ctx.author.voice is None:
+            await ctx.send("you're not in a voice channel")
+        voice_channel = ctx.author.voice.channel
+        if ctx.voice_client is None:
+            await voice_channel.connect()
+        else:
+            await ctx.voice_client.move_to(voice_channel)
         vc = ctx.voice_client
         ctx.voice_client.stop()
         FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options' : '-vn'}
@@ -35,8 +41,6 @@ class music_cog(commands.Cog):
                 'preferredquality': '192',
             }],
         }
-    
-
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url,download = False)
             url2 = info['formats'][0]['url']
