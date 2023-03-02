ad1026cb5e91fd765f0c6bf1b65adee571d5856f
GOGOJO <joshualu76@gmail.com>
2023-03-02 08:48:04 -0800

    final commit

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from itertools import cycle
 from discord.ext import commands, tasks
 import asyncio
 import tokens
-from yt_dlp import YoutubeDL
+
+
 
 
 client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
@@ -22,34 +23,11 @@ async def on_ready():
 async def change_status():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=next(status)))
 
-
-@client.command()
-async def omg(ctx):
- 
-    ctx.voice_client.play(omg.mp3)
-    
-
 @client.command()
 async def hi(ctx):
     await ctx.send('hi')
-    print('test')
 
-@client.command()
-async def play2(ctx, url):
-    if ctx.author.voice is None:
-            await ctx.send("you're not in a voice channel")
-    voice_channel = ctx.author.voice.channel
-    if ctx.voice_client is None:
-        await voice_channel.connect()
-    else:
-        await ctx.voice_client.move_to(voice_channel)
-    YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
-    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-    with YoutubeDL(YDL_OPTIONS) as ydl:
-        info = ydl.extract_info(url,download =False)
-        url2 = info['formats'][0]['url']
-        source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
-        ctx.play(source)
+
 
 async def main():
     async with client:
@@ -58,3 +36,4 @@ async def main():
         await client.start(tokens.token)
 
 asyncio.run(main())
+
